8163e9f27372f38348498a75528d88d803702c18
Dmitry Vlasov <lazyniv@gmail.com>
2020-01-24 22:04:19 +0700

    Exceptions print to stderr

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -36,7 +36,7 @@ public class Main {
                     System.out.println(calculator.evaluate(expression));
                 }
             } catch (CalculatorException e) {
-                System.out.println(e.getMessage());
+                System.err.println(e.getMessage());
             }
         }
     }
